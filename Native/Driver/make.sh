d846348dc3062255d73a231d194a674fdf043d74
Andreas Vogler <andreas.vogler@me.com>
2017-08-30 11:19:34 +0200

    API_ROOT should be set by user

--- Native/Driver/make.sh
+++ Native/Driver/make.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+if [ ! -d "../Builds" ]; then
+  mkdir ../Builds
+fi 
+
+if [ "$API_ROOT" = "" ]
+then
+  echo "env var \$API_ROOT is not set"
+  exit 1
+fi
+
 . ./make.env
 make clean
 make
