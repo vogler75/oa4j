780b2918519839303f23423aa46aac692302ab20
vogler75 <bmw320ix>
2017-10-23 23:49:38 +0200

    Clojure Example

--- Clojure/src/at/rocworks/oa4j/core.clj
+++ Clojure/src/at/rocworks/oa4j/core.clj
@@ -1,41 +1,55 @@
 (ns at.rocworks.oa4j.core
   (:import (at.rocworks.oa4j.base JClient JManager IHotLink)))
 
-; clojure style functions
+; clojure style functions for oa4j
 (defn hl [f] (reify IHotLink (hotlink [this hotlink] (f hotlink))))
 
 (defn dpConnect [dps callback]
-  (let [x (JClient/dpConnect)]
-    (doall (map #(.add x (name %)) dps))
-    (.hotlink x (hl callback))
-    (.connect x)))
+  (let [f (JClient/dpConnect)]
+    (doall (map #(.add f (name %)) dps))
+    (.hotlink f (hl callback))
+    (.connect f)))
 
 (defn dpSet
-  ([map] (let [x (JClient/dpSet)]
-            (doall (map #(.add x (name (first %)) (last %)) map))
-            (.send x)))
-  ([dp value] (JClient/dpSet (name dp) value)))
+  ([kv] (let [f (JClient/dpSet)]
+            ;(doall (map #(.add x (name (first %)) (last %)) kv))
+            (doall (map (fn [[k v]] (.add f (name k) v)) kv))
+            (.send f)))
+  ([k v] (JClient/dpSet (name k) v)))
 
-(defn dpGet [dp]
-  (if (sequential? dp)
-    (let [res (JClient/dpGet (map #(name %) dp))]
+(defn dpGet [dps]
+  (if (or (set? dps) (sequential? dps))
+    (let [res (JClient/dpGet (map #(name %) dps))]
       (map #(.getValueObject %) res))
-    (let [var (JClient/dpGet (name dp))]
+    (let [var (JClient/dpGet (name dps))]
       (.getValueObject var))))
 
-; test clojure style
-(defn callback [dpIdValueList]
-  (let [v (reduce #(+ %1 %2) (map #(.getValueObject %) dpIdValueList))];
+; callback function sums up all values and set the sum to a dp
+(defn callback [xs]
+  (let [v (reduce #(+ %1 %2) (map #(.getValueObject %) xs))];
     (dpSet :ExampleDP_Trend1. v)))
 
+; manager instance
 (def manager (new JManager))
 
+; main
 (defn -main [& args]
+  ;(-> manager (.init (into-array args)) (.start))
   (.init manager (into-array args))
   (.start manager)
-  (dpSet {:ExampleDP_Arg1. 2.0 :ExampleDP_Arg2. 3.0})
-  (println (clojure.string/join "," (dpGet [:ExampleDP_Arg1. :ExampleDP_Arg2.])))
+
+  ; dpGet
+  (println "dpGet with str: " (dpGet :ExampleDP_Arg1.))
+  (println "dpGet with set: " (dpGet #{:ExampleDP_Arg1. :ExampleDP_Arg2.}))
+  (println "dpGet with seq: " (dpGet [:ExampleDP_Arg1. :ExampleDP_Arg2.]))
+
+  ; dpGet and dpSet
+  (let [[a1 a2] (dpGet [:ExampleDP_Arg1. :ExampleDP_Arg2.])]
+    (dpSet {:ExampleDP_Arg1. (+ 1 a1) :ExampleDP_Arg2. (+ 1 a2)}))
+
+  ; dpConnect
   (let [c (dpConnect [:ExampleDP_Arg1. :ExampleDP_Arg2.] callback)]
     (Thread/sleep 180000)
     (.disconnect c))
+
   (.stop manager))
