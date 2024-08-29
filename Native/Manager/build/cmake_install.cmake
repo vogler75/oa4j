2430ceb6fe64bfb53eff976d1463c0375c8e48f3
Tomasz Madej <tomasz.madej101@gmail.com>
2024-08-29 12:55:46 +0200

    adjust manager build

--- Native/Manager/build/cmake_install.cmake
+++ Native/Manager/build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/WinCC_OA_API")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/WinCC_OA_API")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
